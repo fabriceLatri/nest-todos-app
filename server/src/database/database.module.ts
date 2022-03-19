01e025466f9c04eac27c8dc9d03cdb1736675ca4
Fabrice L <fabrice.latri@hotmail.fr>
2022-03-19 18:36:33 +0100

    Added docker backend

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { rootCertificates } from 'tls';
+
+const url = process.env.MONGO_URL || 'localhost';
+
+console.log(url);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI || '', {
-      user: process.env.MONGO_USER,
-      pass: process.env.MONGO_PASSWORD,
+    MongooseModule.forRoot('mongodb://mongodb:27017', {
+      user: 'root',
+      pass: 'pass12345',
       dbName: 'todos',
       w: 'majority',
       retryWrites: true,
